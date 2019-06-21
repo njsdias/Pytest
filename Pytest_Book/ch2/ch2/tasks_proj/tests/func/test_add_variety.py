2a82c0e2563be9afcb325034d9f1f60a344ecc83
njsdias <njsdias@gmail.com>
2019-06-21 18:49:31 +0100

    Add main files

---
+++ Pytest_Book/ch2/ch2/tasks_proj/tests/func/test_add_variety.py
@@ -0,0 +1,132 @@
+"""Test the tasks.add() API function."""
+
+import pytest
+import tasks
+from tasks import Task
+
+
+def equivalent(t1, t2):
+    """Check two tasks for equivalence."""
+    # Compare everything but the id field
+    return ((t1.summary == t2.summary) and
+            (t1.owner == t2.owner) and
+            (t1.done == t2.done))
+
+# To make sure the database is accessible
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+    yield
+    tasks.stop_tasks_db()
+
+
+
+# Task element types : [summary: str, owner: str, done: bool, id: int]
+def test_add_1():
+    """tasks.get() using id returned from add() works."""
+    task = Task('breathe', 'BRIAN', True)
+    task_id = tasks.add(task)      # gives us the id of the task
+    t_from_db = tasks.get(task_id) # with the id of the task gives all parameters (summary,owner,done)
+    # everything but the id should be the same
+    assert equivalent(t_from_db, task)
+
+
+
+# We can use
+# @pytest.mark.parametrize(argnames, argvalues) to pass lots of data through the same
+# test, like this:
+@pytest.mark.parametrize('task',
+                         [Task('sleep', done=True),
+                          Task('wake', 'brian'),
+                          Task('breathe', 'BRIAN', True),
+                          Task('exercise', 'BrIaN', False)])
+def test_add_2(task):
+    """Demonstrate parametrize with one parameter."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+
+
+# This use of parametrize() works for our purposes. However, let’s pass in the
+# tasks as tuples to see how multiple test parameters would work:
+@pytest.mark.parametrize('summary, owner, done',
+                         [('sleep', None, False),
+                          ('wake', 'brian', False),
+                          ('breathe', 'BRIAN', True),
+                          ('eat eggs', 'BrIaN', False),
+                          ])
+def test_add_3(summary, owner, done):
+    """Demonstrate parametrize with multiple parameters."""
+    task = Task(summary, owner, done)
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+
+# Now let’s go back to the list of tasks version, but move the task list to a variable
+# outside the function:
+tasks_to_try = (Task('sleep', done=True),
+                Task('wake', 'brian'),
+                Task('wake', 'brian'),
+                Task('breathe', 'BRIAN', True),
+                Task('exercise', 'BrIaN', False))
+
+
+@pytest.mark.parametrize('task', tasks_to_try)
+def test_add_4(task):
+    """Slightly different take."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+
+
+# we can use the ids optional parameter to
+# parametrize() to make our own identifiers for each task data set. The ids parameter
+# needs to be a list of strings the same length as the number of data sets.
+# However, because we assigned our data set to a variable name, tasks_to_try, we
+# can use it to generate ids:
+task_ids = ['Task({},{},{})'.format(t.summary, t.owner, t.done)
+            for t in tasks_to_try]
+
+
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+def test_add_5(task):
+    """Demonstrate ids."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+@pytest.mark.parametrize('task', [
+    pytest.param(Task('create'), id='just summary'),
+    pytest.param(Task('inspire', 'Michelle'), id='summary/owner'),
+    pytest.param(Task('encourage', 'Michelle', True), id='summary/owner/done')])
+def test_add_6(task):
+    """Demonstrate pytest.param and id."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+# You can apply parametrize() to classes as well. When you do that, the same data
+# sets will be sent to all test methods in the class:
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+class TestAdd():
+    """Demonstrate parametrize and test classes."""
+
+    def test_equivalent(self, task):
+        """Similar test, just within a class."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert equivalent(t_from_db, task)
+
+    def test_valid_id(self, task):
+        """We can use the same data for multiple tests."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert t_from_db.id == task_id
